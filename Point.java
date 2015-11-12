f48af7d6ff4d5da5ad92d4c13e69269c868fb642
cwmalin <cwmalin@crimson.ua.edu>
2015-11-12 12:09:19 -0600

    Create Point.java

---
+++ Point.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chessengine1;
+
+/**
+ *
+ * @author Connor Malin
+ */
+public class Point {
+    public int row, col;
+    Point(int row, int col){
+        this.row=row;
+        this.col=col;
+    }
+}
